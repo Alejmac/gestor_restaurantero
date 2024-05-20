cd91c48c21c03767940f8f471a72d7c1dbf0af99
Alejandro <alejmac2508@gmail.com>
2024-05-19 22:24:55 -0600

    Se agregaron los paneles

--- main.py
+++ main.py
@@ -20,5 +20,45 @@ etiqueta_titul = Label (panel_superior,text = 'Sistema de facturacion',fg= 'azur
                         font = ('Diosis' , 58 ) , bg = "burlywood4" , width= 27)
 etiqueta_titul.grid(row=0, column=0)
 
+# panel izquierdo
+panel_izquierdo = Frame (aplicacion ,bd = 1, relief= FLAT)
+panel_izquierdo.pack(side= Left)
+
+#panel costos
+panel_costos = Frame (panel_izquierdo ,bd = 1, relief= FLAT)
+panel_costos.pack(side=BOTTOM)
+
+#panel costos
+panel_comidas = LabelFrame(panel_izquierdo , Text= "comida", font=('Dosis', 19 ,'bodl' ), bd =1 ,relief= FLAT,figure= 'azure4')
+panel_comidas.pack(side=LEFT)
+
+#panel bebidas
+panel_bebidads = LabelFrame(panel_izquierdo , Text= "Bebidas", font=('Dosis', 19 ,'bodl' ), bd =1 ,relief= FLAT,figure= 'azure4')
+panel_bebidads.pack(side=LEFT)
+
+#panel Postres
+panel_Postres = LabelFrame(panel_izquierdo , Text= "Postres", font=('Dosis', 19 ,'bodl' ), bd =1 ,relief= FLAT,figure= 'azure4')
+panel_Postres.pack(side=LEFT)
+
+#panel derecho
+panel_derecho = Frame(aplicacion, bd=1, relief= FLAT)
+panel_derecho.pack(side= RIGHT)
+
+#panel calculadora
+panel_calculadora = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_calculadora.pack()
+
+#panel recibo
+panel_recibo = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_recibo.pack()
+
+#panel calculadora
+panel_calculadora = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_calculadora.pack()
+
+#panel botones
+panel_botones = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_botones.pack()
+
 #evitar que la app se ceirre 
 aplicacion.mainloop() 
