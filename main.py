16e335326edb7455a8853427eeb1edc924188df9
Alejandro <alejmac2508@gmail.com>
2024-05-19 23:05:27 -0600

    Se arreglaron los bugs

--- main.py
+++ main.py
@@ -21,23 +21,23 @@ etiqueta_titul = Label (panel_superior,text = 'Sistema de facturacion',fg= 'azur
 etiqueta_titul.grid(row=0, column=0)
 
 # panel izquierdo
-panel_izquierdo = Frame (aplicacion ,bd = 1, relief= FLAT)
-panel_izquierdo.pack(side= Left)
+panel_izquierdo = Frame(aplicacion ,bd = 1, relief= FLAT)
+panel_izquierdo.pack(side= LEFT)
 
 #panel costos
 panel_costos = Frame (panel_izquierdo ,bd = 1, relief= FLAT)
 panel_costos.pack(side=BOTTOM)
 
 #panel costos
-panel_comidas = LabelFrame(panel_izquierdo , Text= "comida", font=('Dosis', 19 ,'bodl' ), bd =1 ,relief= FLAT,figure= 'azure4')
+panel_comidas = LabelFrame(panel_izquierdo , text= "comida", font=('Dosis', 19 ,'bold' ), bd =1 ,relief= FLAT,fg= 'azure4')
 panel_comidas.pack(side=LEFT)
 
 #panel bebidas
-panel_bebidads = LabelFrame(panel_izquierdo , Text= "Bebidas", font=('Dosis', 19 ,'bodl' ), bd =1 ,relief= FLAT,figure= 'azure4')
+panel_bebidads = LabelFrame(panel_izquierdo , text= "Bebidas", font=('Dosis', 19 ,'bold' ), bd =1 ,relief= FLAT,fg= 'azure4')
 panel_bebidads.pack(side=LEFT)
 
 #panel Postres
-panel_Postres = LabelFrame(panel_izquierdo , Text= "Postres", font=('Dosis', 19 ,'bodl' ), bd =1 ,relief= FLAT,figure= 'azure4')
+panel_Postres = LabelFrame(panel_izquierdo , text= "Postres", font=('Dosis', 19 ,'bold' ), bd =1 ,relief= FLAT,fg= 'azure4')
 panel_Postres.pack(side=LEFT)
 
 #panel derecho
@@ -45,20 +45,31 @@ panel_derecho = Frame(aplicacion, bd=1, relief= FLAT)
 panel_derecho.pack(side= RIGHT)
 
 #panel calculadora
-panel_calculadora = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_calculadora = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='burlywood' )
 panel_calculadora.pack()
 
 #panel recibo
-panel_recibo = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_recibo = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='burlywood' )
 panel_recibo.pack()
 
 #panel calculadora
-panel_calculadora = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_calculadora = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='burlywood' )
 panel_calculadora.pack()
 
 #panel botones
-panel_botones = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='butlywood' )
+panel_botones = Frame(panel_derecho,bd=1 , relief=FLAT,bg ='burlywood' )
 panel_botones.pack()
+
+#Lista de comidas
+Lista_comidas = ['pollo','cordero','salmon','merluza','kebab', 'pizza']
+Lista_debidas =['agua','refresco','jugo', 'cola']
+Lista_postres =['helado','fruta','bro','brownies','flan']
+
+contador = 0 
+for comida in Lista_comidas: 
+    comida = Checkbutton(panel_comidas,text= comida.title(), font=('dosis', 19 , 'bold') , onvalue= 1 , offvalue = 0 )
+    comida.grid(row=contador,column=0, sticky= W)
+    contador+=1
 
 #evitar que la app se ceirre 
 aplicacion.mainloop() 
